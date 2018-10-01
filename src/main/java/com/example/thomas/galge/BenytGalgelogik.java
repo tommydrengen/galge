6048e24c1475ce54d0d09aa848d69a2a67f3937c
tommydrengen <thomashalkiernicolajsen@gmail.com>
2018-10-01 10:52:55 +0200

    gl

--- src/main/java/com/example/thomas/galge/BenytGalgelogik.java
+++ src/main/java/com/example/thomas/galge/BenytGalgelogik.java
@@ -3,11 +3,13 @@ package com.example.thomas.galge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BenytGalgelogik extends AppCompatActivity {
+    Galgelogik gl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gl = new Galgelogik();
     }
 }
